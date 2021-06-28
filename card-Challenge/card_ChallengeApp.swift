10e0eca73aeb359e158bbc67743277018e087a72
Justin Matthew Newman <56941007+JustinMatthewNewman@users.noreply.github.com>
2021-06-28 16:35:46 -0400

    Add files via upload

---
+++ card-Challenge/card_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  card_ChallengeApp.swift
+//  card-Challenge
+//
+//  Created by ∞                                                ζ ( s ) =        ∑n (⅟n^s),    n=1 on 6/27/21.
+//
+
+import SwiftUI
+
+@main
+struct card_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
